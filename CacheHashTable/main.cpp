4b9420b1071ee842ef304bc369e53c851ac32199
caesiumCode <caesiumcode@aol.com>
2023-07-16 16:23:59 +0200

    new seed

--- CacheHashTable/main.cpp
+++ CacheHashTable/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, const char * argv[])
     std::string hash = std::string(argv[3]);
     
     if      (hash == "std") select_model<std::hash<std::string>>(argv);
-    else if (hash == "xor") select_model<XorHash<335946>>(argv);
+    else if (hash == "xor") select_model<XorHash<21500544>>(argv);
     else if (hash == "wyh") select_model<WyHash>(argv);
     else std::cout << "UNKNOWN HASH [" << hash << "]" << std::endl;
     
