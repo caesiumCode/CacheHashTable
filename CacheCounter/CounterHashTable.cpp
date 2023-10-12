cf6d6fa9eaf95e9342643faf9f5bb3f2ef8389a2
caesiumCode <caesiumcode@aol.com>
2023-10-12 19:12:02 +1100

    try better naive log counter

--- CacheCounter/CounterHashTable.cpp
+++ CacheCounter/CounterHashTable.cpp
@@ -162,6 +162,8 @@ uint64_t CounterHashTable<HashT, P>::rng()
 template<typename HashT, uint8_t P>
 uint8_t CounterHashTable<HashT, P>::log_increment(uint8_t counter)
 {
+    return counter + (rng() % (LOG_INV[counter+1] - LOG_INV[counter]) == 0);
+    
     if constexpr(P == 4)
     {
         return counter +
